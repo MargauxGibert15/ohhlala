49b0e85da2608f24d285740e33fd869ffd55bb1b
margauxgibert15 <margaux.gibert15@hotmail.fr>
2019-10-04 12:00:09 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -55,7 +55,7 @@ require('./passport')(app)
 app.use('/api', require('./routes/index'))
 app.use('/api', require('./routes/auth'))
 app.use('/api', require('./routes/pattern'))
-
+console.log('hello')
 // For any routes that starts with "/api", catch 404 and forward to error handler
 app.use('/api/*', (req, res, next) => {
   let err = new Error('Not Found')
